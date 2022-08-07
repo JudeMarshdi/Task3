727a88d64acdc7b645e4b4877a864faa534184be
JudeMarshdi <109734102+JudeMarshdi@users.noreply.github.com>
2022-08-07 21:02:40 +0300

    Add files via upload

---
+++ get.php
@@ -0,0 +1,28 @@
+<?php
+
+$data = $_GET ["d"];
+echo $data;
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sensor";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO sensordata (sensor_data)
+VALUES ('$data')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
